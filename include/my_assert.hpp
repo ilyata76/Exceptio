c5a8bd9dfb5438edce3829ee66a1cd9aafc772df
ilyata76 <ilyata76@yandex.ru>
2022-04-21 03:34:23 +0300

    comments for constructors of assert

--- include/my_assert.hpp
+++ include/my_assert.hpp
@@ -5,21 +5,35 @@
 
 #include "exceptio.hpp"
 
+// based on EXCEPTIO_HPP
 class Assert {
 	public:
+		// COMMENT: "It was caused by using Assert"
 		Assert(bool condition) {
 			if (!condition) throw Exceptio(INT_PLUG, STR_PLUG, "It was caused by using Assert");
 		}
+
+		// Your DECRIPTION when calling an Exceptio
+		// COMMENT: "It was caused by using Assert"
 		Assert(bool condition, const char* description) {
-			if (!condition) throw Exceptio(INT_PLUG, description, STR_PLUG);
+			if (!condition) throw Exceptio(INT_PLUG, description, "It was caused by using Assert");
 		}
+
+		// Your DESCRIPTION and COMMENT when calling an Exception
+		Assert(bool condition, const char* description, const char* comment) {
+			if (!condition) throw Exceptio(INT_PLUG, description, comment);
+		}
+
+		// Your INDEX, DESCRIPTION when calling and Exception
+		// COMMENT: "It was caused by using Assert"
 		Assert(bool condition, int16_t index, const char* description) {
 			if (!condition) throw Exceptio(index, description, "It was caused by using Assert");
 		}
+
+		// Your INDEX, DESCRIPTION and COMMENT when calling and Exception
 		Assert(bool condition, int16_t index, const char* description, const char* comment) {
 			if (!condition) throw Exceptio(index, description, comment);
 		}
 };
-
 
 #endif // !MY_ASSERT_HPP
